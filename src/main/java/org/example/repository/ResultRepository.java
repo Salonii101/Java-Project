b2710a463bbb2be4a4cd121998577ae7e222c5b0
sukritcharredsoul <sukritchoudhary16@gmail.com>
2025-09-28 10:45:41 +0530

    completed Services v1

--- src/main/java/org/example/repository/ResultRepository.java
+++ src/main/java/org/example/repository/ResultRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ResultRepository extends JpaRepository<Result,Integer> {
-    List<Result> findAll();
     List<Result> findByUserId(String userId);
     List<Result> findByQuizId(int quizId);
 }
